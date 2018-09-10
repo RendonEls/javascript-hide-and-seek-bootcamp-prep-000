c595a56bdfa4ad458796176b6cac22e788a4d22b
Rendon Elsesser <rendonelsesser@gmail.com>
2018-09-10 00:37:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ function increaseRankBy(n){
 }
 
 function increaseRankby(n){
-  var rank = document.querySelectorALL("ul.ranked-list.li")
-  for (i=0; i<ranks.length; i++){
-  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  var rank = document.querySelectorALL("ul.ranked-list.li");
+  for (var i=0; i<ranks.length; i++){
+  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
   }
 }
 
 function deepestChild() {
   var grandNode = document.querySelector('grand-node')
-  var deepNode = grandNode.children[0]
-  
+  var deepNode = grandNode.children[0];
+  for (var i = 0; !deepNode.children[i]; i){
+    deepNode = deepNode.children[0];
+  }
+ return deepNode; 
 }
